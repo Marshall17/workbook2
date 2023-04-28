10b15f37c81435d6e983bf34447261fe748bf1d2
Marshal Rai <kusmet722@gmail.com>
2023-04-28 13:01:42 -0400

    exersice calculator

---
+++ excersises/helloworldwebsite/scripts/calculator.js
@@ -0,0 +1,13 @@
+let number1Field = document.getElementById ("number1Field")
+
+number1Field.Number(2)
+
+let number2 = document.getElementById ("number2Field")
+let answer = document.getElementById ("answerField")
+
+function addBtnClick() {
+   let onAddBtnClicked = document.getElementById ("onAddBtnClicked")
+    let addNumb = onAddBtnClicked.value
+
+    let message= `( ${addNumb})`
+}
